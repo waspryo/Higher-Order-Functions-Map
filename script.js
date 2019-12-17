426efe0d2bc83c0e6a0217bdfd7b0b2a457895e2
waspryo <wasp2123321@gmail.com>
2019-12-17 22:58:06 +0900

    substring

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-let numbers = [1,2,3]
-let doubledNumbers = [] // 1. initialize accumulate variable
+// let numbers = [1,2,3]
+// let doubledNumbers = [] // 1. initialize accumulate variable
 
 
-numbers.forEach((number) => {
-    doubledNumbers.push(number * 2)
+// numbers.forEach((number) => {
+//     doubledNumbers.push(number * 2)
+// })
+// console.log(doubledNumbers)
+
+// let numbers = [1, 2, 3]
+
+// let doubledNumbers = numbers.map((number, index, array) => {
+//     console.log(number, 'is the', index, 'in the array:',array )
+//     return number * 2
+// })
+
+// console.log(doubledNumbers)
+
+const numbers = [13, 42, 1337]
+
+const doubledNumbers = numbers.map(number => {
+    return number * 2
 })
 console.log(doubledNumbers)
+
+
+
+const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+]
+const getWeekdays = days.map(weeekday => {
+    return weeekday.substring(0, 3)
+})
+console.log(getWeekdays)
