0be057536f5c15b13ec3237bf05a2989b8a50a8a
waspryo <wasp2123321@gmail.com>
2019-12-17 22:34:39 +0900

    map example 1

---
+++ script.js
@@ -0,0 +1,8 @@
+let numbers = [1,2,3]
+let doubledNumbers = [] // 1. initialize accumulate variable
+
+
+numbers.forEach((number) => {
+    doubledNumbers.push(number * 2)
+})
+console.log(doubledNumbers)
